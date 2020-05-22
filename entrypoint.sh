8f37bbd73dfdd2f1c1ab27a7789c67b027333be2
jer-k <jeremyk112@gmail.com>
2020-05-22 11:58:10 -0700

    Formatted with Postman

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,19 @@
 
 set -e
 
-curl -XPOST \
+curl -X POST "https://e9451f76.ngrok.io/api/environments" \
      -H "X-Account-ID: $RELEASE_ACCOUNT_ID" \
      -H "X-Account-Token: $RELEASE_ACCOUNT_AUTHENTICATION_TOKEN" \
      --silent \
      --show-error \
      --fail \
-     -d "{
-  \"app_id\": \"$INPUT_APP_ID\", 
-  \"branch\": \"$INPUT_BRANCH\", 
-  \"services\": \"$INPUT_SERVICES\", 
-  \"commit_short\": \"$INPUT_COMMIT_SHORT\", 
-  \"commit_long\": \"$INPUT_COMMIT_LONG\", 
-  \"committer\": \"$INPUT_COMMITTER\", 
-  \"commit_message\": \"$INPUT_COMMIT_MESSAGE\", 
-  \"commit_date\": \"$INPUT_COMMIT_DATE\", 
-  \"logs\": \"$INPUT_LOGS\"
-}" "https://e9451f76.ngrok.io/api/environments"     
+     --form "app_id=$INPUT_APP_ID" \
+     --form "branch=$INPUT_BRANCH" \
+     --form "services=$INPUT_SERVICES" \
+     --form "commit_short=$INPUT_COMMIT_SHORT" \
+     --form "commit_long=$INPUT_COMMIT_LONG" \
+     --form "committer=$INPUT_COMMITTER" \
+     --form "commit_message=$INPUT_COMMIT_MESSAGE" \
+     --form "commit_date=$INPUT_COMMIT_DATE" \
+     --form "logs=$INPUT_LOGS"
+     
