c09f16f6aeb3550f57fccc3df03549c1c7a7eea4
jer-k <jeremyk112@gmail.com>
2020-05-22 11:22:54 -0700

    Still don't know bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,27 +2,20 @@
 
 set -e
 
-generate_post_data()
-{
-  cat <<EOF
-{
-  "app_id": "$INPUT_APP_ID", 
-  "branch": "$INPUT_BRANCH", 
-  "services": "$INPUT_SERVICES", 
-  "commit_short": "$INPUT_COMMIT_SHORT", 
-  "commit_long": "$INPUT_COMMIT_LONG", 
-  "committer": "$INPUT_COMMITTER", 
-  "commit_message": "$INPUT_COMMIT_MESSAGE", 
-  "commit_date": "$INPUT_COMMIT_DATE", 
-  "logs": "$INPUT_LOGS"
-}
-EOF
-}
-
-curl -H "X-Account-ID: $RELEASE_ACCOUNT_ID" \
+curl -XPOST \
+     -H "X-Account-ID: $RELEASE_ACCOUNT_ID" \
      -H "X-Account-Token: $RELEASE_ACCOUNT_AUTHENTICATION_TOKEN" \
      --silent \
      --show-error \
      --fail \
-     -d "$(generate_post_data)" \
-     -X POST https://e9451f76.ngrok.io/api/environments
+     -d "{
+  \"app_id\": \"$INPUT_APP_ID\", 
+  \"branch\": \"$INPUT_BRANCH\", 
+  \"services\": \"$INPUT_SERVICES\", 
+  \"commit_short\": \"$INPUT_COMMIT_SHORT\", 
+  \"commit_long\": \"$INPUT_COMMIT_LONG\", 
+  \"committer\": \"$INPUT_COMMITTER\", 
+  \"commit_message\": \"$INPUT_COMMIT_MESSAGE\", 
+  \"commit_date\": \"$INPUT_COMMIT_DATE\", 
+  \"logs\": \"$INPUT_LOGS\"
+}" "https://e9451f76.ngrok.io/api/environments"     
